8f5f4ba6006bd0cab41a5144e793c563e9c67085
Adalto Selau Sparremberger <assparremberger@senacrs.com.br>
2026-09-16 11:47:11 -0300

    Aula05 - Pilha - LIFO - Exercício

---
+++ Aula05 - Pilha - LIFO/Exercício/main.py
@@ -0,0 +1,12 @@
+#Exercício:
+# Construa um aplicativo para uma pilha de livros
+# em que o livro possui título, autor e quantidade de páginas
+# Devem ser construídos os seguintes métodos:
+# 1) Adicionar livro na pilha
+# 2) Remover um livro da pilha
+# 3) Imprimir a pilha de livros
+# 4) Retornar a posição de um livro na pilha, informando o título
+# 5) Retornar os livros que estão na pilha, mas de um autor que o usuário informar
+# 0) Sair
+# Construa um menu de opções com as opções acima citadas
+# O aplicativo só termina quando o usuário escolher a opção 0
